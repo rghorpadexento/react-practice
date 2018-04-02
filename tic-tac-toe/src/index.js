2ba098308cc1d0f46c6d485fe9d7828aba7a0f80
Rohit R. Ghorpade <rghorpade@dc2.propsol.org>
2018-04-02 14:43:05 +0530

    tic tact toe - completed extra parts

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -2,47 +2,75 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+{
+  /* function used to render single square.
+  Actually it was declared as Class previously but later changed to function
+  Because it is not doing anything else rather than rendering the single square
+If you have a class with simple render element you can change it to function for better use */
+}
 function Square (props) {
   return (
-    <button className='square' onClick={props.onClick}>
+    <button
+      className='square'
+      onClick={props.onClick}
+      style={{ color: props.color }}
+    >
       {props.value}
     </button>
   )
 }
 
+{
+  /* Board class having some logic of rendering each square and creating board using for loops
+It is internally using Square component/function to render the elements
+It's a stateless component as there is no state defined */
+}
 class Board extends React.Component {
-  renderSquare (i) {
+  renderSquare (i, color) {
     return (
       <Square
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
+        color={color}
       />
     )
+  }
+
+  createBoard = () => {
+    let row = []
+    var counter = 0
+    const [first, second, third] = this.props.colorSquares
+      ? this.props.colorSquares
+      : ''
+    for (let i = 0; i < 3; i++) {
+      let col = []
+      for (let j = 0; j < 3; j++) {
+        const color = second &&
+          (counter == first || counter == second || counter == third)
+          ? 'blue'
+          : ''
+
+        col.push(this.renderSquare(counter, color))
+        counter++
+      }
+      row.push(<div className='board-row'>{col}</div>)
+    }
+    return row
   }
 
   render () {
     return (
       <div>
-        <div className='board-row'>
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className='board-row'>
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className='board-row'>
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {this.createBoard()}
       </div>
     )
   }
 }
 
+{
+  /* This is Main statefull component which contains and transfers the props to other components.
+  Internally calls Board component to render board each time user clicks on square */
+}
 class Game extends React.Component {
   constructor (props) {
     super(props)
@@ -54,12 +82,13 @@ class Game extends React.Component {
         }
       ],
       xIsNext: true,
-      stepNumber: 0
+      stepNumber: 0,
+      moveOrder: true
     }
   }
 
   handleClick (i) {
-    const history = this.state.history
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
     const current = history[history.length - 1]
     const squares = current.squares.slice()
     const valRowCol = getRowCol(i)
@@ -75,61 +104,66 @@ class Game extends React.Component {
   }
 
   jumpTo (step) {
-    if (step === 0) {
-      this.setState({
-        history: [
-          {
-            squares: Array(9).fill(null),
-            valueRowCol: Array(2).fill(null)
-          }
-        ],
-        xIsNext: true,
-        stepNumber: 0
-      })
-    } else {
-      this.setState({
-        stepNumber: step,
-        xIsNext: step % 2 === 0
-      })
-    }
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0
+    })
+  }
+
+  reverse () {
+    const order = this.state.moveOrder
+    this.setState({ moveOrder: !order })
   }
 
   render () {
     const history = this.state.history
-    console.log('History =>', history)
     const current = history[this.state.stepNumber]
-    console.log('Current =>', current)
     const winner = calculateWinner(current.squares)
 
     const moves = history.map((step, move) => {
-      console.log('step', step)
-      console.log('move', move)
+      const isCurrentMove = !!(this.state.stepNumber === move && move)
+      const colorMove = isCurrentMove ? 'Blue' : ''
       const desc = move
         ? 'Go to Move #' + move + '    =>    ' + step.valRowCol
         : 'Go to game start'
       return (
         <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          <button
+            style={{ color: colorMove }}
+            onClick={() => this.jumpTo(move)}
+          >
+            {desc}
+          </button>
         </li>
       )
     })
-    console.log('Moves', moves)
-    console.log('============')
+    const newMoves = this.state.moveOrder ? moves : moves.reverse()
+    const order = this.state.moveOrder ? 'Descending Moves' : 'Ascending Moves'
+
     let status
     if (winner) {
-      status = 'Winner :' + winner
+      status = 'Winner :' + winner.winner
+    } else if (this.state.stepNumber === 9) {
+      status = 'Match Draw'
     } else {
       status = 'Next Player:' + (this.state.xIsNext ? 'X' : 'O')
     }
-
+    const colorSquares = winner && winner.squares ? winner.squares : null
     return (
       <div className='game'>
         <div className='game-board'>
-          <Board squares={current.squares} onClick={i => this.handleClick(i)} />
+          <Board
+            squares={current.squares}
+            colorSquares={colorSquares}
+            onClick={i => this.handleClick(i)}
+          />
         </div>
         <div className='game-info'>
+          <div>
+            <button onClick={() => this.reverse()}>{order}</button>
+          </div>
           <div>{status}</div>
-          <div>{moves}</div>
+          <div>{newMoves}</div>
         </div>
       </div>
     )
@@ -146,14 +180,17 @@ function calculateWinner (squares) {
     [1, 4, 7],
     [2, 4, 6],
     [2, 5, 8],
-    [3, 4, 6],
+    [3, 4, 5],
     [6, 7, 8]
   ]
 
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i]
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a]
+      return {
+        winner: squares[a],
+        squares: lines[i]
+      }
     }
   }
   return null
